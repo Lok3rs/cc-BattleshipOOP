f19d455876ef3ee281e93a0fdceb96cd2b85658e
Dawid Adamski <lok3rsss@gmail.com>
2021-03-10 20:54:48 +0100

    ShipType enum designed

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,12 +1,13 @@
 package com.codecool;
 
+import com.codecool.board.ShipType;
 import com.codecool.board.Square;
 import com.codecool.board.SquareStatus;
 
 public class Main {
 
     public static void main(String[] args) {
-        Square square = new Square(10, 10, SquareStatus.MISSED);
-        System.out.println(square.getSquareCharacter());
+        ShipType shipType = ShipType.Carrier;
+        System.out.println(shipType.getShipLength());
     }
 }
