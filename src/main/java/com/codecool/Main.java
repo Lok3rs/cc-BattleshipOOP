4b221857941db70e3d7663cebc7173fffe946729
Dawid Adamski <lok3rsss@gmail.com>
2021-03-11 10:36:03 +0100

    Logo designed

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -3,11 +3,12 @@ package com.codecool;
 import com.codecool.board.ShipType;
 import com.codecool.board.Square;
 import com.codecool.board.SquareStatus;
+import com.codecool.utils.Display;
 
 public class Main {
 
     public static void main(String[] args) {
-        ShipType shipType = ShipType.Carrier;
-        System.out.println(shipType.getShipLength());
+        Display display = new Display();
+        display.printMenu();
     }
 }
