41391b5e0f8f81ce8ba6d0411eed2342a8aca746
kasluk345 <kaszuba.lukasz@gmail.com>
2021-03-11 14:24:14 +0100

    basic shooting implemented, isAlive method added, minor changes in SquareStatus class

--- src/main/java/com/codecool/player/HumanPlayer.java
+++ src/main/java/com/codecool/player/HumanPlayer.java
@@ -1,11 +1,13 @@
 package com.codecool.player;
 
 import com.codecool.board.Ship;
+import com.codecool.board.ShipCollection;
 
 import java.util.List;
 
 public class HumanPlayer extends Player {
-    public HumanPlayer(int id, String name, int score, List<Ship> ships, boolean isAlive) {
-        super(id, name, score, ships, isAlive);
+    public HumanPlayer(int id, String name, int score, boolean isAlive, ShipCollection ships) {
+        super(id, name, score, isAlive, ships);
     }
 }
+
--- src/main/java/com/codecool/player/Player.java
+++ src/main/java/com/codecool/player/Player.java
@@ -3,6 +3,8 @@ package com.codecool.player;
 import com.codecool.board.Ship;
 import com.codecool.board.ShipCollection;
 import com.codecool.board.Square;
+import com.codecool.board.SquareStatus;
+import com.codecool.utils.Display;
 import com.codecool.utils.Input;
 
 import java.util.ArrayList;
@@ -12,17 +14,56 @@ public abstract class Player {
     private int id;
     private String name;
     private int score;
-    protected List<Ship> ships;
     protected boolean isAlive = true;
+    protected ShipCollection ships;
     private Input input = new Input();
+    private Display display = new Display();
 
 
-    public Player(int id, String name, int score, List<Ship> ships, boolean isAlive){
+    public Player(int id, String name, int score, boolean isAlive, ShipCollection ships){
         this.id = id;
         this.name = name;
         this.score = score;
-        this.ships = ships;
         this.isAlive = isAlive;
+        this.ships = ships;
+
+    }
+
+
+    private void handleShoot(Input input, ShipCollection ships) {
+        List<Ship> shipsToCheck = ships.getShips();
+
+        for (Ship ship : shipsToCheck) {
+            for (Square square : ship.getShipContent()) {
+                SquareStatus status = square.getSquareStatus();
+                switch (status) {
+                    case EMPTY:
+                        System.out.println("you missed");
+                        square.setSquareStatus(SquareStatus.MISSED);
+                    case SHIP:
+                        System.out.println("You hit a ship");
+                        square.setSquareStatus(SquareStatus.HIT);
+                    case HIT:
+                        System.out.println("You already shoot here");
+                    case MISSED:
+                        System.out.println("You missed");
+                        System.out.println("You already shoot here");
+                }
+            }
+        }
+    }
+
+    private void checkIsAlive(ShipCollection ships){
+        List<Ship> shipsToCheck = ships.getShips();
+        int shipsCount = 0;
+
+        for (Ship ship : shipsToCheck) {
+            shipsCount++;
+        }
+
+        if (shipsCount == 0) {
+            setAlive(false);
+        }
     }
 
     public String getName() {
@@ -31,14 +72,6 @@ public abstract class Player {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<Ship> getShips() {
-        return ships;
-    }
-
-    public void setShips(List<Ship> ships) {
-        this.ships = ships;
     }
 
     public boolean isAlive() {
