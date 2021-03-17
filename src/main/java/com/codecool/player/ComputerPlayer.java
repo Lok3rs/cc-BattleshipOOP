4141688327ffc0be6b261725bbe5e528085d9a6f
kasluk345 <kaszuba.lukasz@gmail.com>
2021-03-17 19:04:51 +0100

    ComputerPlayerEasy implemented

--- src/main/java/com/codecool/player/ComputerPlayer.java
+++ src/main/java/com/codecool/player/ComputerPlayer.java
@@ -1,6 +1,13 @@
 package com.codecool.player;
 
+import java.util.Random;
+
 public abstract class ComputerPlayer extends Player{
 
+    public int getRandomNumber(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(10) -1;
+        return randomNumber;
+    }
 }
 
