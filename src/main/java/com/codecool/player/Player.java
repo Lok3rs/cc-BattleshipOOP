b7680299c96e31c5069cf74dd98a77a191eefef5
Dawid Adamski <lok3rsss@gmail.com>
2021-03-19 11:10:12 +0100

    Remove isAlive and removeShip methods from Player class

--- src/main/java/com/codecool/player/Player.java
+++ src/main/java/com/codecool/player/Player.java
@@ -1,9 +1,7 @@
 package com.codecool.player;
 
 import com.codecool.board.Board;
-import com.codecool.board.Ship;
 import com.codecool.board.ShipCollection;
-import com.codecool.board.Square;
 import com.codecool.highscores.Highscore;
 import com.codecool.utils.Display;
 import com.codecool.utils.Input;
@@ -21,14 +19,6 @@ public abstract class Player {
 
     public Player() {
         this.name = input.getPlayerName();
-    }
-
-    public void removeShip(Ship ship){
-        ships.removeShip(ship);
-    }
-
-    public boolean isAlive(){
-        return ships.getShips().size() > 0;
     }
 
     public String getName(){
